49a495d5c4c3e882e02f6587eb1020c45ba64908
David Sagala <davidsagala2140@gmail.com>
2025-09-08 23:15:04 +0700

    finish: easy-object-frequency-distributions

--- easy-object-frequency-distribution.js
+++ easy-object-frequency-distribution.js
@@ -13,3 +13,15 @@ getFrequencies([true, false, true, false, false]) // { true: 2, false: 3 }
 
 getFrequencies([]) // {}
 */
+
+const getFrequencies = (arr, res = {}) => {
+  for (let i = 0; i <= arr.length - 1; i++) {
+    res[arr[i]] ? res[arr[i]]++ : (res[arr[i]] = 1);
+  }
+  return res;
+};
+
+console.log(getFrequencies(["A", "B", "A", "A", "A"])); // { A: 4, B: 1 }
+console.log(getFrequencies([1, 2, 3, 3, 2])); // { "1": 1, "2": 2, "3": 2 }
+console.log(getFrequencies([true, false, true, false, false])); // { true: 2, false: 3 }
+console.log(getFrequencies([])); // {}
