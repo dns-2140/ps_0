06129a60072f295aae838e02f8fddd95a04ed0c5
David Sagala <davidsagala2140@gmail.com>
2025-09-11 01:32:23 +0700

    Finish hard-object-battle-stats.js

--- hard-object-battle-stats.js
+++ hard-object-battle-stats.js
@@ -40,3 +40,53 @@ Output :
 }
 
 */
+
+const battleStats = (data) => {
+  const res = {};
+  for (let i = 0; i <= data.length - 1; i++) {
+    //check if general exists
+    if (res[data[i][0]]) {
+      console.log("general exists");
+      //check if number is positive
+      if (data[i][1] > 0) {
+        //check if menang exist
+        if (res[data[i][0]]["Menang"]) {
+          res[data[i][0]]["Menang"] += data[i][1];
+        } else {
+          res[data[i][0]]["Menang"] = data[i][1];
+        }
+      } else {
+        if (res[data[i][0]]["Kalah"]) {
+          res[data[i][0]]["Kalah"] += Math.abs(data[i][1]);
+        } else {
+          res[data[i][0]]["Kalah"] = Math.abs(data[i][1]);
+        }
+      }
+    } else {
+      res[data[i][0]] = {
+        Kalah: 0,
+        Menang: 0,
+      };
+
+      if (data[i][1] > 0) {
+        res[data[i][0]]["Menang"] = Math.abs(data[i][1]);
+      } else {
+        res[data[i][0]]["Kalah"] = Math.abs(data[i][1]);
+      }
+    }
+  }
+
+  return res;
+};
+
+let battle = [
+  ["Hwang Shi Mok", -1],
+  ["Savants", 1],
+  ["Hwang Shi Mok", 2],
+  ["Hwang Shi Mok", 1],
+  ["Savants", 5],
+  ["Legolas", 10],
+  ["Legolas", -20],
+  ["Legolas", -10],
+];
+console.log(battleStats(battle));
